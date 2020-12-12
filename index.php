ba06504458312802b6e3eaafabef3bbd2135ac89
Diego Lombardi <69874329+DiegoLombard@users.noreply.github.com>
2020-12-12 15:16:51 -0300

    Cria_Index_

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diego Lombard</title>
+</head>
+<body>
+    <h1>Página Principal de Diego Lombard </h1>
+</body>
+</html>
